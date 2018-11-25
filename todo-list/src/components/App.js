1f522618eccb19643904c9abe8c563205b6c9e94
robbin <gooodev402@gmail.com>
2018-11-26 02:08:34 +0900

    add state

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -3,11 +3,72 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 class App extends Component {
+  state ={
+    input: '',
+    todos: [
+      {id: 0, text: '리액트 공부하기', done:true},
+      {id: 1, text: '컴포넌트 스타일링 해보기', done:false},
+    ]
+  }
+
+  id=1;
+  getId = ()=>{
+    return ++this.id;
+  }
+
+  handleChange = (e) =>{
+    const {value} = e.target;
+
+    this.setState({
+      input : value
+    })
+  }
+
+  handleInstert = () =>{
+    const {input, todos} = this.state;
+
+    const newTodo = {
+      id: this.getId(),
+      text: input,
+      doen: false
+    }
+
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    })
+  }
+
+  handleToggle = (id) =>{
+    const {todos} = this.state;
+    const index = todos.findIndex(todo => todo.id=== id)
+
+    const toggled = {
+      ...todos[index],
+      done: !todos[index].done
+    }
+
+    this.setState({
+      todos: [
+        ...todos.slice(0,index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    })
+  }
+
   render() {
+    const {input, todos} = this.state;
+    const {
+      handleChange,
+      handleInsert,
+      handleToggle
+    } = this;
+
     return (
       <PageTemplate>
-        <TodoInput/>
-        <TodoList/>
+        <TodoInput onChange={handleChange} value={input} onInsert={handleInsert}/>
+        <TodoList todos={todos} OnToggle={handleToggle}/>
       </PageTemplate>
     );
   }
--- todo-list/src/components/TodoList/TodoList.js
+++ todo-list/src/components/TodoList/TodoList.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
-import TodoItem from '../TodoItem'
+import React, { Component } from "react";
+import TodoItem from "../TodoItem";
 
-class TodoList extends Component{
-    render(){
-        return(
-            <div>
-                <TodoItem done>리액트 공부하기</TodoItem>
-                <TodoItem >컴포넌트 스타일링 해보기</TodoItem>
-            </div>
-        )
-    }
+class TodoList extends Component {
+  render() {
+    const { todos, onToggle } = this.props;
+
+    const todoList = todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        done={todo.done}
+        onToggle={() => onToggle(todo.id)}
+      >
+        {todo.text}
+      </TodoItem>
+    ));
+    return <div>{todoList}</div>;
+  }
 }
 
 export default TodoList;
