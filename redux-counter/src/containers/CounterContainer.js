901e68d48507ff8f43925913cd612625c807e5f9
robbin <gooodev402@gmail.com>
2018-11-30 13:44:23 +0900

    first redux project is completed

---
+++ redux-counter/src/containers/CounterContainer.js
@@ -0,0 +1,53 @@
+import Counter from "../components/Counter";
+import * as actions from "../actions";
+import { connect } from "react-redux";
+
+export function getRandomColor() {
+  const colors = [
+    "#495057",
+    "#f03e3e",
+    "#d6336c",
+    "#ae3ec9",
+    "#7048e8",
+    "#4263eb",
+    "#1c7cd6",
+    "#1098ad",
+    "#0ca678",
+    "#37b24d",
+    "#74b816",
+    "#f59f00",
+    "#f76707"
+  ];
+
+  const random = Math.floor(Math.random() * 13);
+
+  return colors[random];
+}
+
+// state를 받아, 컴포넌트의 props로 사용할 객체를 반환한다.
+const mapStateToProps = state => {
+  return {
+    color: state.color,
+    number: state.number
+  };
+};
+
+// dispatch를 받아 액션을 디스패치하는 함수들을 객체안에 넣어서 반환한다.
+const mapDispatchToPros = dispatch => {
+  return {
+    onIncrement: () => dispatch(actions.increment()),
+    onDecrement: () => dispatch(actions.decrement()),
+    onSetColor: () => {
+      const color = getRandomColor();
+      dispatch(actions.setColor(color));
+    }
+  };
+};
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToPros,
+  //mergeProps(잘 안쓴다!!) : state와 dispatch가 동시에 필요한 함수를 props로 전달할 때 사용.
+)(Counter);
+
+export default CounterContainer;
