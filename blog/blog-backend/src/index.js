20854c4df2af86def3123ab9dd6d04a026f8a0fc
robbin <gooodev402@gmail.com>
2018-12-03 22:49:09 +0900

    middleware next() is Promise Object and async/await usage

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,11 +1,30 @@
-const Koa = require('koa');
+const Koa = require("koa");
 
 const app = new Koa();
 
-app.use((ctx)=>{
-    ctx.body = "hello World";
-})
+// app.use((ctx, next) => {
+//   console.log(1);
+//   next().then(()=>{
+//   console.log("bye");
+//   });
+// });
 
-app.listen(4000, ()=>{
-    console.log('listening to port 4000')
-})
+// Async / Await를 쓰면, 콜백을 사용하지 않아도 된다.
+// 위와 동일한 의미를 가진 코드.
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log("bye"); // 미들웨어들의 모든 작업이 끝나고 실행된다.
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+app.use((ctx, next) => {
+  ctx.body = "hello World";
+});
+
+app.listen(4000, () => {
+  console.log("listening to port 4000");
+});
