d6194e6120f37d8920a1ec49e5f66a57026c1a30
robbin <gooodev402@gmail.com>
2018-12-03 22:37:23 +0900

    blog-backend setting with eslint and koa

---
+++ blog/blog-backend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules" : {
+        "no-unused-vars": 1, // 경고
+        "comma-dangle": 0, //do nothing
+        "eol-last": 0,
+        "no-console": 0,
+    }
+};
