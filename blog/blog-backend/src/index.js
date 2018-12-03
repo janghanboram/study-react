dc1e54f0284b425183dc1c1d32cd4f2b698f1b13
robbin <gooodev402@gmail.com>
2018-12-03 23:01:37 +0900

    koa-router example

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,30 +1,30 @@
-const Koa = require("koa");
+const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa();
+const router = new Router();
 
-// app.use((ctx, next) => {
-//   console.log(1);
-//   next().then(()=>{
-//   console.log("bye");
-//   });
-// });
+// 라우터를 설정할 때,
+// 첫번째 파라미터 : 라우트의 path,
+// 두번째 파라미터 : 이 라우트를 처리할 함수.
 
-// Async / Await를 쓰면, 콜백을 사용하지 않아도 된다.
-// 위와 동일한 의미를 가진 코드.
-app.use(async (ctx, next) => {
-  console.log(1);
-  await next();
-  console.log("bye"); // 미들웨어들의 모든 작업이 끝나고 실행된다.
-});
+router.get('/', (ctx)=>{
+    ctx.body="홈"
+})
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-app.use((ctx, next) => {
-  ctx.body = "hello World";
-});
+router.get('/about/:name?', (ctx)=>{
+    const {name} = ctx.params;
+    // name의 존재 유무에 따라 다른 결과를 출력한다.
+    ctx.body = name ? `${name}의 소개` : '소개'
+})
 
-app.listen(4000, () => {
-  console.log("listening to port 4000");
-});
+router.get('/posts', (ctx)=>{
+    const {id} = ctx.query;
+    ctx.body = id ? `포스트 ${id}` : '포스트 아이디가 없습니다.'
+})
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, ()=>{
+    console.log('listening to port 4000')
+})
