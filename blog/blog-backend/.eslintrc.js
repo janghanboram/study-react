60c7702cfdb9f3b83101dc67b52a4761087520d8
robbin <gooodev402@gmail.com>
2018-12-04 02:19:15 +0900

    set NODE_PATH

--- blog/blog-backend/.eslintrc.js
+++ blog/blog-backend/.eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules" : {
-        "no-unused-vars": 1, // 경고
-        "comma-dangle": 0, //do nothing
-        "eol-last": 0,
-        "no-console": 0,
+  extends: "airbnb-base",
+  settings: {
+    "import/resolver": {
+      node: { paths: [path.resolve("./src")] }
     }
+  },
+  rules: {
+    "no-unused-vars": 1, // 경고
+    "comma-dangle": 0, //do nothing
+    "eol-last": 0,
+    "no-console": 0
+  }
 };
