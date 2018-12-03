968cdd4aff102fd4683d60060053fddb6ecc78dd
robbin <gooodev402@gmail.com>
2018-12-03 23:24:27 +0900

    start to write REST api : posts

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,10 +1,9 @@
 const Router = require('koa-router')
+const posts = require('./posts')
 
 const api = new Router()
 
-api.get('/test', (ctx)=>{
-    ctx.body="성공"
-})
+api.use('/posts', posts.routes())
 
 // 라우터를 내보낸다.
 module.exports = api;
