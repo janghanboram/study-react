0ba0553d4f8df320bbfd6aaa68857a066c0a8b30
robbin <gooodev402@gmail.com>
2019-01-15 22:40:24 +0900

    create new component - Footer

---
+++ blog/blog-frontend/src/components/common/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Footer.scss";
+import className from "classnames/bind";
+import { Link } from "react-router-dom";
+
+const cx = className.bind(styles);
+
+const Footer = () => (
+  <footer className={cx("footer")}>
+    <Link to="/" className={cx("brand")}>reactBlog</Link>
+
+    <div className={cx("admin-login")}>
+      관리자 로그인
+    </div>
+  </footer>
+);
+
+export default Footer;
