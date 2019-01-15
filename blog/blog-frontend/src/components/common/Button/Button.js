255b93a27773ae85dd0e6081ecc25303a45ca928
robbin <gooodev402@gmail.com>
2019-01-15 23:37:53 +0900

    Put the button in the Header. bug fix - button rendering error

--- blog/blog-frontend/src/components/common/Button/Button.js
+++ blog/blog-frontend/src/components/common/Button/Button.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import styles from './Button.scss'
-import classNames from 'classnames/bind'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./Button.scss";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 //NOTE: JSX에서 ...을 사용하면, 내부에 있는 값을 props로 넣어줌
-const Div = ({children, ...rest})=> <div> {...rest}>{children}</div>
+const Div = ({children, ...rest}) => <div {...rest}>{children}</div>
 
 const Button = ({
-        children, to, onClick, disabled, theme = 'defualt',
-}) =>{
+        children, to, onClick, disabled, theme = 'default',
+}) => {
     const Element = (to && !disabled) ? Link : Div;
 
     return (
