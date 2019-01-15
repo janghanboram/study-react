4e7966e8da67d9294673fbeca8642d1719cf704f
robbin <gooodev402@gmail.com>
2019-01-15 23:30:13 +0900

    create new component - Button

---
+++ blog/blog-frontend/src/components/common/Button/Button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Button.scss'
+import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom'
+
+const cx = classNames.bind(styles);
+
+//NOTE: JSX에서 ...을 사용하면, 내부에 있는 값을 props로 넣어줌
+const Div = ({children, ...rest})=> <div> {...rest}>{children}</div>
+
+const Button = ({
+        children, to, onClick, disabled, theme = 'defualt',
+}) =>{
+    const Element = (to && !disabled) ? Link : Div;
+
+    return (
+        //NOTE: disabled가 true이면 disabled 클래스를 추가함.
+        //NOTE: 비활성화 되면 onClick 실행 안함.
+        <Element
+            to={to}
+            className={cx('button', theme, {disabled})}
+            onClick={disabled ? () => null : onClick}>
+            {children}
+        </Element>
+    )
+} 
+
+export default Button;
