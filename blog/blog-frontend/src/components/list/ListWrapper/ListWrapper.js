43660d9af9c7dea0c955b42ca77c855a3214020b
robbin <gooodev402@gmail.com>
2019-01-15 23:45:42 +0900

    create new component - ListWrapper

---
+++ blog/blog-frontend/src/components/list/ListWrapper/ListWrapper.js
@@ -0,0 +1,17 @@
+/*
+ * 내부 내용을 페이지 한가운데 정렬
+ * 위.아래 패딩 설정
+ * 브라우저 크기에 따라 화면 크기 조정
+ */
+
+import React from "react";
+import styles from "./ListWrapper.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+const ListWrapper = ({ children }) => (
+  <div className={cx("list-wrapper")}>{children}</div>
+);
+
+export default ListWrapper;
